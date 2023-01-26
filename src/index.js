0a2847998af1f458fb6f323ad365b021f0d7dac2
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-27 01:18:43 +0300

    feat: start solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [ '(', '[', '{' ];
+  const closeBrackets = [ ')', ']', '}' ];
+
+  const strArr = str.split('');
+  if (strArr.length % 2 !== 0 || closeBrackets.includes(strArr[0]) || openBrackets.includes(strArr[strArr.length - 1])) {
+      return false;
+  } else {
+      return true;
+  }
 }
