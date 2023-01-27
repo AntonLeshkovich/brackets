f3ac8716ff211e02102ff116fbfbde671ede2843
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-28 00:04:09 +0300

    fix: fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  const openBrackets = ['(', '[', '{', '2', '4', '6'];
+  const openBrackets = ['(', '[', '{', '1', '3', '5'];
   const otherBrackets = ['|', '7', '8'];
   const pairBrackets = {
       [')']: '(',
       [']']: '[',
       ['}']: '{',
-      ['1']: '2',
-      ['3']: '4',
-      ['5']: '6',
+      ['2']: '1',
+      ['4']: '3',
+      ['6']: '5',
   };
 
-    let stack = [];
-    
-    for (let i = 0; i < str.length; i++) {
+  let stack = [];
+  
+  for (let i = 0; i < str.length; i++) {
 
-        let currentSymbol = str[i];
+      let currentSymbol = str[i];
 
-        if(openBrackets.includes(currentSymbol)) {
-            stack.push(currentSymbol);
-        } else if (stack.includes(currentSymbol) && otherBrackets.includes(currentSymbol)) {
-            stack.pop(currentSymbol);
-        } else if (otherBrackets.includes(currentSymbol)) {
-            stack.push(currentSymbol);
-        } else if (stack.length === 0) {
-            return false;
-        }
+      if(openBrackets.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+      } else if (stack.includes(currentSymbol) && otherBrackets.includes(currentSymbol)) {
+          stack.pop(currentSymbol);
+      } else if (otherBrackets.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+      } else if (stack.length === 0) {
+          return false;
+      }
 
-        let topStackEl = stack[stack.length - 1];
+      let topStackEl = stack[stack.length - 1];
 
-        if (pairBrackets[currentSymbol] === topStackEl) {
-            stack.pop();
-        }
-        console.log(stack);
-    }
-    if (stack.length === 0) {
-        return true;
-    } else {
-        return false;
-    }
+      if (pairBrackets[currentSymbol] === topStackEl) {
+          stack.pop();
+      }
+      console.log(stack);
+  }
+  if (stack.length === 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
