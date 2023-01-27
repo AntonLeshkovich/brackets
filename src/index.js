f0e8ec14fce9f9b333a7fb14ef743fb4c58c7176
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-27 23:54:31 +0300

    fix: update solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  const openBrackets = ['(', '[', '{', '1', '3', '5'];
+  const openBrackets = ['(', '[', '{', '2', '4', '6'];
   const otherBrackets = ['|', '7', '8'];
   const pairBrackets = {
       [')']: '(',
@@ -10,32 +10,32 @@ module.exports = function check(str, bracketsConfig) {
       ['5']: '6',
   };
 
-  let stack = [];
-  
-  for (let i = 0; i < str.length; i++) {
+    let stack = [];
+    
+    for (let i = 0; i < str.length; i++) {
 
-      let currentSymbol = str[i];
+        let currentSymbol = str[i];
 
-      if(openBrackets.includes(currentSymbol)) {
-          stack.push(currentSymbol);
-      } else if (stack.includes(currentSymbol) && otherBrackets.includes(currentSymbol)) {
-          stack.pop(currentSymbol);
-      } else if (otherBrackets.includes(currentSymbol)) {
-          stack.push(currentSymbol);
-      } else if (stack.length === 0) {
-          return false;
-      }
+        if(openBrackets.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+        } else if (stack.includes(currentSymbol) && otherBrackets.includes(currentSymbol)) {
+            stack.pop(currentSymbol);
+        } else if (otherBrackets.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+        } else if (stack.length === 0) {
+            return false;
+        }
 
-      let topStackEl = stack[stack.length - 1];
+        let topStackEl = stack[stack.length - 1];
 
-      if (pairBrackets[currentSymbol] === topStackEl) {
-          stack.pop();
-      }
-      console.log(stack);
-  }
-  if (stack.length === 0) {
-      return true;
-  } else {
-      return false;
-  }
+        if (pairBrackets[currentSymbol] === topStackEl) {
+            stack.pop();
+        }
+        console.log(stack);
+    }
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
