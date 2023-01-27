4658c75e4b4fe129d974fb8954a4dc0851cbfcd8
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-27 17:44:47 +0300

    feat: update solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str[1]) {
-    return true;
-  } else {
-    return false;
-  }
+  const openBrackets = ['(', '[', '{', '1', '3', '5'];
+    const pairBrackets = {
+        [')']: '(',
+        [']']: '[',
+        ['}']: '{',
+        ['|']: '|',
+        ['1']: '2',
+        ['3']: '4',
+        ['5']: '6',
+    };
+
+    let stack = [];
+    
+    for (let i = 0; i < str.length; i++) {
+
+        let currentSymbol = str[i];
+
+        if(openBrackets.includes(currentSymbol)) {
+            stack.push(currentSymbol);
+        } else if (stack.length === 0) {
+            return false;
+        }
+
+        let topStackEl = stack[stack.length - 1];
+
+        if (pairBrackets[currentSymbol] === topStackEl) {
+            stack.pop();
+        }
+    }
+    if (stack.length === 0) {
+        return true;
+    }
 }
