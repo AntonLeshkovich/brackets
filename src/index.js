508038df50fd83916985f43ac78d1d114aa817fc
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-27 22:53:10 +0300

    feat: update solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
-  const openBrackets = ['(', '[', '{', '1', '3', '5', '7', '8', '|'];
+  const openBrackets = ['(', '[', '{', '1', '3', '5'];
+  const otherBrackets = ['|', '7', '8'];
   const pairBrackets = {
       [')']: '(',
       [']']: '[',
@@ -7,33 +8,34 @@ module.exports = function check(str, bracketsConfig) {
       ['1']: '2',
       ['3']: '4',
       ['5']: '6',
-      ['7']: '7',
-      ['8']: '8',
-      ['|']: '|',
   };
 
-    let stack = [];
-    
-    for (let i = 0; i < str.length; i++) {
+  let stack = [];
+  
+  for (let i = 0; i < str.length; i++) {
 
-        let currentSymbol = str[i];
+      let currentSymbol = str[i];
 
-        if(openBrackets.includes(currentSymbol)) {
-            stack.push(currentSymbol);
-        } else if (stack.length === 0) {
-            return false;
-        }
+      if(openBrackets.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+      } else if (stack.includes(currentSymbol) && otherBrackets.includes(currentSymbol)) {
+          stack.pop(currentSymbol);
+      } else if (otherBrackets.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+      } else if (stack.length === 0) {
+          return false;
+      }
 
-        let topStackEl = stack[stack.length - 1];
+      let topStackEl = stack[stack.length - 1];
 
-        if (pairBrackets[currentSymbol] === topStackEl) {
-            stack.pop();
-        }
-        // console.log(stack);
-    }
-    if (stack.length === 0) {
-        return true;
-    } else {
-        return false;
-    }
+      if (pairBrackets[currentSymbol] === topStackEl) {
+          stack.pop();
+      }
+      console.log(stack);
+  }
+  if (stack.length === 0) {
+      return true;
+  } else {
+      return false;
+  }
 }
